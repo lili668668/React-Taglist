899d9695acdb70bc1233d3674a32adb814da1bac
Tzu-Yin Hong <lili668668@gmail.com>
2017-04-19 15:18:00 +0800

    Add ToolBar

---
+++ app/Components/ToolBar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class ToolBar extends Component {
+    static get height() {
+        return 5;
+    }
+
+    static get css() {
+        return {
+            backgroundColor: "yellow",
+            height: this.height + 'vw'
+        }
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div style={ToolBar.css}>
+                Hello World
+            </div>
+        );
+    }
+}
+
+export default ToolBar;
